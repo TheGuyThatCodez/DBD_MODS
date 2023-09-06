2aad6747c31cb59be040732f68f0577d4fcde20f
TheGuyThatCodez <134526423+TheGuyThatCodez@users.noreply.github.com>
2023-09-05 20:08:45 -0700

    Update source.lua

--- source.lua
+++ source.lua
@@ -37,47 +37,9 @@ function DBD_GetFigure()
     return workspace.ActiveEntities:FindFirstChild("Figure")
 end
 
-DBD_Templates = {
-    Rush = {Model = game.ReplicatedStorage.Entities.Rush,PlaySounds = {"Distant","Near"},Wobble = true,Speed = 1,Kills = true,Rebound = false},
-    Ambush = {Model = game.ReplicatedStorage.Entities.Ambush,PlaySounds = {"Static","Near"},Wobble = false,Speed = 1,Kills = true,Rebound = {2,6}},
-}
-
-function DBD_SpawnRushlike(settings) -- Model model, PlaySounds {Sound}, Wobble true/false, Speed number, Kills true/false, Rebound {min,max},
-    local rushclone = settings.Model:Clone()
-    rushclone.Parent = workspace.ActiveEntities
-    local points={} --table of part points to go through in order
-    local movePart=rushclone --rush/ambush part
-
-    print("Playing PlaySounds")
-
-    if settings.PlaySounds then
-        local Children = rushclone:GetChildren()
-        for i=1,#Children do
-            if table.find(settings.PlaySounds,Children[i].Name) then
-                Children[i]:Play()
-            end
-        end
-    end
-
-    print("Wobbling PlaySounds")
-
-    if settings.Wobble == nil then
-        settings.Wobble = false
-    end
-
-    if settings.Wobble then --Basically only vanilla rush uses this, otherwise useless
-        task.spawn(function()
-            while true do
-                if rushclone then
-                    movePart.Distant.SoundGroup.Pitch.Octave = math.random(50,70)/100
-                end
-                wait(0.4)
-            end
-        end)
-    end
-    
-    print("Getting The Moves")
-    
+local inside_DoorOpened = Instance.new("BindableEvent")
+DBD_DoorOpened = inside_DoorOpened.Event
+workspace.CurrentRoom.Changed:Connect(function(value)
     local segments = workspace.Segments:GetChildren()
     table.sort(segments, function(a,b)
         if a:FindFirstChild("SegmentNumValue") and b:FindFirstChild("SegmentNumValue") then
@@ -86,95 +48,155 @@ function DBD_SpawnRushlike(settings) -- Model model, PlaySounds {Sound}, Wobble 
             return false
         end
     end)
-    iSTART = workspace.CurrentRoom.Value-10
-    if iSTART < 1 then
-        iSTART = 1
-    end
-    for i=iSTART,#segments do
-        print(segments[i].Name)
-        if segments[i]:FindFirstChild("RushPoints") then
-            local temp = segments[i].RushPoints:GetChildren()
-            table.sort(temp, function(a,b)
-                return tonumber(a.Name) < tonumber(b.Name)
-            end)
-            for i=1,#temp do
-                points[#points+1] = temp[i].CFrame.Position
-            end
-        end
-        points[#points+1] = segments[i].Exit.Main.CFrame.Position
-    end
+    inside_DoorOpened:Fire(value,segments[value])
+)
+
+DBD_Templates = {
+    Rush = {Model = game.ReplicatedStorage.Entities.Rush,PlaySounds = {"Distant","Near"},Wobble = true,Speed = 1,Kills = true,Rebound = false},
+    Ambush = {Model = game.ReplicatedStorage.Entities.Ambush,PlaySounds = {"Static","Near"},Wobble = false,Speed = 1,Kills = true,Rebound = {2,6}},
+}
+
+function DBD_SpawnRushlike(settings) -- Model model, PlaySounds {Sound}, Wobble true/false, Speed number, Kills true/false, Rebound {min,max},
+    --Events: OnTouch (hiding true/false)
+    local rushclone = settings.Model:Clone()
+    local events = {Instance.new("BindableEvent",Parent = rushclone)}
     
+    task.spawn(function(settings)
+        
+        rushclone.Parent = workspace.ActiveEntities
+        local points={} --table of part points to go through in order
+        local movePart=rushclone --rush/ambush part
     
-
-    print("Moving The Moves & Killing The Players")
-
-    if settings.Kills == nil then
-        settings.Kills = false
-    end
-
-    task.spawn(function()
-        if not settings.Kills then
-            return -1
-        end
-        while rushclone.Parent == workspace.ActiveEntities do
-            wait()
-            local rayOrigin = rushclone.Position
-            local rayDirection = CFrame.lookAt(rayOrigin,game.Players.LocalPlayer.Character.Head.Position).LookVector * 15
-        
-            rushclone.CFrame = CFrame.lookAt(rayOrigin,game.Players.LocalPlayer.Character.Head.Position) 
-        
-            local raycastParams = RaycastParams.new()
-            raycastParams.FilterDescendantsInstances = {rush}
-            raycastParams.FilterType = Enum.RaycastFilterType.Blacklist
-            raycastParams.RespectCanCollide = true
-        
-            local raycastResult = workspace:Raycast(rayOrigin, rayDirection,raycastParams)
-        
-            if raycastResult then
-                if raycastResult.Instance.Parent:FindFirstChild("Humanoid") then
-                    if not game.Players.LocalPlayer:GetAttribute("Hidden") then
-                        DBD_DieBasic()
-                    end
+        print("Playing PlaySounds")
+    
+        if settings.PlaySounds then
+            local Children = rushclone:GetChildren()
+            for i=1,#Children do
+                if table.find(settings.PlaySounds,Children[i].Name) then
+                    Children[i]:Play()
                 end
+            end
+        end
+    
+        print("Wobbling PlaySounds")
+    
+        if settings.Wobble == nil then
+            settings.Wobble = false
+        end
+    
+        if settings.Wobble then --Basically only vanilla rush uses this, otherwise useless
+            task.spawn(function()
+                while true do
+                    if rushclone then
+                        movePart.Distant.SoundGroup.Pitch.Octave = math.random(50,70)/100
+                    end
+                    wait(0.4)
+                end
+            end)
+        end
+        
+        print("Getting The Moves")
+        
+        local segments = workspace.Segments:GetChildren()
+        table.sort(segments, function(a,b)
+            if a:FindFirstChild("SegmentNumValue") and b:FindFirstChild("SegmentNumValue") then
+                return tonumber(a.SegmentNumValue.Value+1) < tonumber(b.SegmentNumValue.Value+1)
             else
-                    
+                return false
+            end
+        end)
+        iSTART = workspace.CurrentRoom.Value-10
+        if iSTART < 1 then
+            iSTART = 1
+        end
+        for i=iSTART,#segments do
+            print(segments[i].Name)
+            if segments[i]:FindFirstChild("RushPoints") then
+                local temp = segments[i].RushPoints:GetChildren()
+                table.sort(temp, function(a,b)
+                    return tonumber(a.Name) < tonumber(b.Name)
+                end)
+                for i=1,#temp do
+                    points[#points+1] = temp[i].CFrame.Position
+                end
+            end
+            points[#points+1] = segments[i].Exit.Main.CFrame.Position
+        end
+        
+        
+    
+        print("Moving The Moves & Killing The Players")
+    
+        if settings.Kills == nil then
+            settings.Kills = false
+        end
+    
+        task.spawn(function()
+            while rushclone.Parent == workspace.ActiveEntities do
+                wait()
+                local rayOrigin = rushclone.Position
+                local rayDirection = CFrame.lookAt(rayOrigin,game.Players.LocalPlayer.Character.Head.Position).LookVector * 15
+            
+                rushclone.CFrame = CFrame.lookAt(rayOrigin,game.Players.LocalPlayer.Character.Head.Position) 
+            
+                local raycastParams = RaycastParams.new()
+                raycastParams.FilterDescendantsInstances = {rush}
+                raycastParams.FilterType = Enum.RaycastFilterType.Blacklist
+                raycastParams.RespectCanCollide = true
+            
+                local raycastResult = workspace:Raycast(rayOrigin, rayDirection,raycastParams)
+            
+                if raycastResult then
+                    if raycastResult.Instance.Parent:FindFirstChild("Humanoid") and raycastResult.Instance.Parent == game.Players.LocalPlayer.Character then
+                        if not game.Players.LocalPlayer:GetAttribute("Hidden") then
+                            if not settings.Kills then
+                                return DBD_DieBasic()
+                            end
+                            
+                        end
+                        events[1]:Fire(game.Players.LocalPlayer:GetAttribute("Hidden"))
+                    end
+                else
+                        
+                end
+            end
+        end)
+    
+        if settings.Speed == nil then
+            settings.Speed = 1
+        end
+        
+        local speed=60*settings.Speed
+        
+        local tweenService=game:GetService("TweenService")
+        rushclone.CFrame=CFrame.new(points[1])
+        if not settings.Rebound then
+            for i,v in pairs(points) do
+              local d=(rushclone.CFrame.Position-v).Magnitude
+              local t=d/speed
+               tweenService:Create(rushclone,TweenInfo.new(t,Enum.EasingStyle.Linear),{CFrame=CFrame.new(v)}):Play()
+              task.wait(t)
+            end
+        else
+            for r=1,math.random(settings.Rebound[1],settings.Rebound[2]) do
+                for i=1,#points do
+                    v = points[i]
+                    local d=(rushclone.CFrame.Position-v).Magnitude
+                    local t=d/speed
+                    tweenService:Create(rushclone,TweenInfo.new(t,Enum.EasingStyle.Linear),{CFrame=CFrame.new(v)}):Play()
+                    task.wait(t)
+                end
+                for i=1,#points do
+                    v = points[#points-(i-1)]
+                    local d=(rushclone.CFrame.Position-v).Magnitude
+                    local t=d/speed
+                    tweenService:Create(rushclone,TweenInfo.new(t,Enum.EasingStyle.Linear),{CFrame=CFrame.new(v)}):Play()
+                    task.wait(t)
+                end
             end
         end
+        
+        rushclone:Destroy()
     end)
-
-    if settings.Speed == nil then
-        settings.Speed = 1
-    end
-    
-    local speed=60*settings.Speed
-    
-    local tweenService=game:GetService("TweenService")
-    rushclone.CFrame=CFrame.new(points[1])
-    if not settings.Rebound then
-        for i,v in pairs(points) do
-          local d=(rushclone.CFrame.Position-v).Magnitude
-          local t=d/speed
-           tweenService:Create(rushclone,TweenInfo.new(t,Enum.EasingStyle.Linear),{CFrame=CFrame.new(v)}):Play()
-          task.wait(t)
-        end
-    else
-        for r=1,math.random(settings.Rebound[1],settings.Rebound[2]) do
-            for i=1,#points do
-                v = points[i]
-                local d=(rushclone.CFrame.Position-v).Magnitude
-                local t=d/speed
-                tweenService:Create(rushclone,TweenInfo.new(t,Enum.EasingStyle.Linear),{CFrame=CFrame.new(v)}):Play()
-                task.wait(t)
-            end
-            for i=1,#points do
-                v = points[#points-(i-1)]
-                local d=(rushclone.CFrame.Position-v).Magnitude
-                local t=d/speed
-                tweenService:Create(rushclone,TweenInfo.new(t,Enum.EasingStyle.Linear),{CFrame=CFrame.new(v)}):Play()
-                task.wait(t)
-            end
-        end
-    end
-    
-    rushclone:Destroy()
+    return events
 end
